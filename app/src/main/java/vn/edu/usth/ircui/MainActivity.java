bcf440ff47563acac244169704436e0b98f4d6a8
eine apple <longvh4021@gmail.com>
2025-10-05 11:52:37 +0700

    Update MainActivity.java

--- app/src/main/java/vn/edu/usth/ircui/MainActivity.java
+++ app/src/main/java/vn/edu/usth/ircui/MainActivity.java
@@ -1,14 +1,22 @@
 package vn.edu.usth.ircui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentTransaction;
+import vn.edu.usth.ircui.function.MessageNotification;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_NOTIFICATION_PERMISSION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,41 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.container, new LoginFragment());
             ft.commit();
+        }
+
+        /*App notification permission
+        * When app started -> ask for notification permission
+        * Agree -> a welcome notification appear
+        * Tap the notification -> open the app */
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                        REQUEST_NOTIFICATION_PERMISSION
+                );
+            } else {
+                MessageNotification.showMsgNotification(
+                        this,
+                        "System",
+                        "Welcome to IRC"
+                );
+            }
+        }
+    }
+    public void onRequestPermissionResult(int requestCode, String[] permission, int[] grantResult){
+        super.onRequestPermissionsResult(requestCode, permission, grantResult);
+
+        if (requestCode == REQUEST_NOTIFICATION_PERMISSION){
+            if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED){
+                MessageNotification.showMsgNotification(
+                        this,
+                        "System",
+                        "Notification permission granted!"
+                );
+            }else {
+                Toast.makeText(this, "Notification permission denied", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
